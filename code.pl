efe24e85afa7c276aa0a52d4e19f4de71791b357
Muhammad Hasnian <125374283+mkhasnain@users.noreply.github.com>
2024-11-14 09:45:44 +0500

    Update code.pl

--- code.pl
+++ code.pl
@@ -1,3 +1,8 @@
+% Traffic condition facts based on car count predictions
+traffic(low, Cars) :- Cars >= 0, Cars =< 5.
+traffic(medium, Cars) :- Cars >= 6, Cars =< 20.
+traffic(high, Cars) :- Cars >= 21.
+
 % Traffic conditions for different time intervals
 traffic_condition(1, Cars, Condition) :- traffic(Condition, Cars).
 traffic_condition(5, Cars, Condition) :- traffic(Condition, Cars).
